13b93af18569fd4ce9cc569dc975758721fda186
Chris <ctomotsugu@sapient.com>
2018-03-16 16:34:18 -0400

    updated files

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -11,7 +11,19 @@
           <img class="blah" alt="responsive image">
         </div>
         <div class="col-12 col-lg-6 px-0 grey-cell">
-          col-12 col-lg-6
+            <div class="row">
+              <div class="col-12 col-lg-6 dark-grey-cell">
+                col 1
+              </div>
+              <div class="col-12 col-lg-6 dark-grey-cell">
+                col 2
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12 col-lg-6 dark-grey-cell">
+                test col
+              </div>
+            </div>
         </div>
         <div class="col-12 col-lg-1 px-0 grey-cell">
           col-12 col-lg-1
@@ -41,7 +53,50 @@
   @import '../../styles/custom-bootstrap.scss';
   @import '../../../node_modules/bootstrap/scss/bootstrap.scss';
 
-// Extra small devices (smart phones, less than 34em)
+  #dashboard {
+    width:100%;
+    height:100px;
+  }
+  
+  .fill{
+    position:relative;
+    display:flex;
+    justify-content:left;
+    align-items:center;
+    overflow:hidden;
+  }
+  .grey-cell {
+    height:calc(100vh - 156px);
+    background-color: #e3e3e3;
+    border-style:solid;
+    border-width: thin;
+    border-color:black;
+  }
+  .dark-grey-cell {
+    height:calc(100vh - 155px);
+    background-color: #5e5c5b;
+    border-style:solid;
+    border-width: thin;
+    border-color:rgb(44, 39, 39);
+  }
+  .blah{
+    position:relative;
+    left:0;
+  }
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+
+  .col-xs-12{
+    background-color:red;
+    width:100%;
+  }
+
+  // Extra small devices (smart phones, less than 34em)
 @media (max-width: 33.9em) { 
 
 }
@@ -71,40 +126,4 @@
     content:url("/src/images/guy_with_tattoos_2.jpg");
   }
 }
-
-  #dashboard {
-    width:100%;
-    height:100px;
-  }
-  
-  .fill{
-    position:relative;
-    display:flex;
-    justify-content:left;
-    align-items:center;
-    overflow:hidden;
-  }
-  .grey-cell {
-    height:calc(100vh - 156px);
-    background-color: #e3e3e3;
-    border-style:solid;
-    border-width: thin;
-    border-color:black;
-  }
-  .blah{
-    position:relative;
-    left:0;
-  }
-  h1, p {
-    text-align: center;
-  }
-
-  p {
-    color: red;
-  }
-
-  .col-xs-12{
-    background-color:red;
-    width:100%;
-  }
 </style>
