adaab2123e14314f4bb350ab2680a1ff6557b114
Chris <ctomotsugu@sapient.com>
2018-03-21 18:05:06 -0400

    updated files

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -62,7 +62,7 @@
         <b-navbar-brand>BootstrapVue</b-navbar-brand>
         <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
         <b-collapse is-nav id="nav_dropdown_collapse">
-          <b-navbar-nav ml-auto>
+          <b-navbar-nav class="ml-auto">
             <b-nav-item href="#">Home</b-nav-item>
             <b-nav-item href="#">Link</b-nav-item>
             <!-- Navbar dropdowns -->
@@ -151,9 +151,13 @@
     background-color: $white;
     width:100%;
   }
-  .b-nav-item {
-    color: $red;
+
+  .navbar-light .navbar-nav .nav-link{
+
+    text-decoration:none;
+    color:#000000;
   }
+
   #nav_dropdown_collapse {
     //margin-top:200px;
     background-color:$white;
